708cdc39febc4730d29b8e010e690a0f53d35013
unknown <zahid@cybernest.com>
2021-11-18 21:20:08 +0500

    integrete theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from './components/common/user/header';
+import Footer from './components/common/user/footer';
+import Index from './components/user';
+import Shop from './components/user/shop';
+import Cart from './components/user/cart';
+import Checkout from './components/user/checkout';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+     <Router>
+        <Routes>
+          <Route exact path="/" element={<Index/>} />
+          <Route exact path="shop" element={<Shop/>} />
+          <Route exact path="cart" element={<Cart/>} />
+          <Route exact path="checkout" element={<Checkout/>} />
+
+        </Routes>
+      </Router>
+
+
+      {/* <Index/> */}
+      <Footer/>
+
     </div>
   );
 }
